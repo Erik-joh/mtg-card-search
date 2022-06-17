b78bfc813e4d7ece216666cb769de72c371e71f9
Erik <erik.johannesson@uptive.se>
2022-06-17 14:46:46 +0200

    CardCarousel inc/dec buttons

--- src/components/organisms/CardsCarousel.js
+++ src/components/organisms/CardsCarousel.js
@@ -3,26 +3,31 @@ import Card from "../molecules/Card";
 import Button from "../atoms/Button";
 import { CardsCarouselContainer } from "../pages/ResultsPage/styles";
 
-const CardsCarousel = ({ cards }) => {
-  return (
-    <CardsCarouselContainer>
-      <div>
-        {cards.map((card) => {
-          return (
-            <Card
-              key={card.id}
-              cardName={card.name}
-              imageUrl={card.image_uris ? card.image_uris.normal : ""}
-            />
-          );
-        })}
-      </div>
-      <Button buttonText="<" />
-      <p>
-        {0}/{cards.length}
-      </p>
-      <Button buttonText=">" />
-    </CardsCarouselContainer>
-  );
+const CardsCarousel = ({
+  card,
+  onIncrement,
+  onDecrement,
+  cardNumber,
+  totalNumberOfCards,
+}) => {
+  if (card) {
+    return (
+      <CardsCarouselContainer>
+        <div>
+          <Card
+            key={card.id}
+            cardName={card.name}
+            imageUrl={card.image_uris ? card.image_uris.normal : ""}
+          />
+        </div>
+        <Button onClick={onDecrement} buttonText="<" />
+        <p>
+          {cardNumber}/{totalNumberOfCards}
+        </p>
+        <Button onClick={onIncrement} buttonText=">" />
+      </CardsCarouselContainer>
+    );
+  }
+  return <CardsCarouselContainer></CardsCarouselContainer>;
 };
 export default CardsCarousel;
--- src/components/pages/ResultsPage/index.js
+++ src/components/pages/ResultsPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { EditSearch, ResultContainer } from "./styles";
@@ -13,67 +13,24 @@ const ResultsPage = (props) => {
   const { generatedCard, cards, errorMsg, loading } = useSelector(
     (state) => state.searchReducer
   );
+  const [currentCarouselIndex, setCurrentCarouselIndex] = useState(0);
   const [colorPercentItems, setColorPercentItems] = useState([]);
   useEffect(() => {
-    setColorPercentItems(CalculateColorPrecent());
+    console.log("useEffect for calculation colors percent");
+    setColorPercentItems(CalculateColorPrecent(cards));
   }, [cards]);
-  /* const colorPercentItems = useMemo(() => CalculateColorPrecent(), [cards]); */
 
-  function CalculateColorPrecent() {
-    //const colorsAmount = { white: 0, blue: 0, black: 0, red: 0, green: 0 };
-    const colorsAmount = {};
-    const totalAmountOfCards = cards.length;
-    cards.forEach((card) => {
-      if (card.colors) {
-        card.colors.forEach((color) => {
-          colorsAmount[color]
-            ? colorsAmount[color]++
-            : (colorsAmount[color] = 1);
-        });
-      }
-    });
-
-    const colorPercentItems = [
-      {
-        percent: percentage(colorsAmount.W, totalAmountOfCards),
-        textColor: "black",
-        backgroundColor: "white",
-        id: 1,
-      },
-      {
-        percent: percentage(colorsAmount.U, totalAmountOfCards),
-        textColor: "white",
-        backgroundColor: "blue",
-        id: 2,
-      },
-      {
-        percent: percentage(colorsAmount.B, totalAmountOfCards),
-        textColor: "white",
-        backgroundColor: "black",
-        id: 3,
-      },
-      {
-        percent: percentage(colorsAmount.R, totalAmountOfCards),
-        textColor: "white",
-        backgroundColor: "red",
-        id: 4,
-      },
-      {
-        percent: percentage(colorsAmount.G, totalAmountOfCards),
-        textColor: "white",
-        backgroundColor: "green",
-        id: 5,
-      },
-    ];
-
-    return colorPercentItems;
+  function onIncrement() {
+    setCurrentCarouselIndex(
+      cards.length - 1 === currentCarouselIndex ? 0 : currentCarouselIndex + 1
+    );
   }
-  function percentage(partialValue, totalValue) {
-    if (partialValue === undefined) return 0;
-    if (totalValue === 0) return 0;
-    const percent = 100 * (partialValue / totalValue);
-    return Math.round(percent);
+  function onDecrement() {
+    setCurrentCarouselIndex(
+      0 === currentCarouselIndex ? cards.length - 1 : currentCarouselIndex - 1
+    );
   }
+
   if (loading) {
     return <Loading loadingText="Loading..." />;
   }
@@ -86,7 +43,13 @@ const ResultsPage = (props) => {
     <PageContainer>
       <ResultContainer>
         <GeneratedCard generatedCard={generatedCard} />
-        <CardsCarousel cards={cards} />
+        <CardsCarousel
+          cardNumber={currentCarouselIndex + 1}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+          card={cards[currentCarouselIndex]}
+          totalNumberOfCards={cards.length}
+        />
         <EditSearch>
           <Link to="/">Edit Search</Link>
         </EditSearch>
@@ -95,5 +58,59 @@ const ResultsPage = (props) => {
     </PageContainer>
   );
 };
+
+function CalculateColorPrecent(cards) {
+  const colorsAmount = {};
+  const totalAmountOfCards = cards.length;
+  cards.forEach((card) => {
+    if (card.colors) {
+      card.colors.forEach((color) => {
+        colorsAmount[color] ? colorsAmount[color]++ : (colorsAmount[color] = 1);
+      });
+    }
+  });
+
+  const colorPercentItems = [
+    {
+      percent: percentage(colorsAmount.W, totalAmountOfCards),
+      textColor: "black",
+      backgroundColor: "white",
+      id: 1,
+    },
+    {
+      percent: percentage(colorsAmount.U, totalAmountOfCards),
+      textColor: "white",
+      backgroundColor: "blue",
+      id: 2,
+    },
+    {
+      percent: percentage(colorsAmount.B, totalAmountOfCards),
+      textColor: "white",
+      backgroundColor: "black",
+      id: 3,
+    },
+    {
+      percent: percentage(colorsAmount.R, totalAmountOfCards),
+      textColor: "white",
+      backgroundColor: "red",
+      id: 4,
+    },
+    {
+      percent: percentage(colorsAmount.G, totalAmountOfCards),
+      textColor: "white",
+      backgroundColor: "green",
+      id: 5,
+    },
+  ];
+
+  return colorPercentItems;
+}
+
+function percentage(partialValue, totalValue) {
+  if (partialValue === undefined) return 0;
+  if (totalValue === 0) return 0;
+  const percent = 100 * (partialValue / totalValue);
+  return Math.round(percent);
+}
 
 export default ResultsPage;
